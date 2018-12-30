a891d0da17c44a5bd243e8d61b585d20d8340daa
Robert Fischer <artiefischer@gmail.com>
2018-12-29 19:48:35 -0700

    one last try for heroku

--- db/dbInit.go
+++ db/dbInit.go
@@ -16,9 +16,18 @@ var ConnStr = DBInit()
 // DBInit initializes the databasw using .env vars
 func DBInit() string {
 	gotenv.Load()
+	var ConnStr string
 	dbname := os.Getenv("DBNAME")
 	dbuser := os.Getenv("DBUSER")
-	ConnStr := fmt.Sprintf("user=%[1]v "+
-		"dbname=%[2]v sslmode=disable", dbuser, dbname)
+	url := os.Getenv("DATABASE_URL")
+
+	if url == "" {
+		ConnStr := fmt.Sprintf("user=%[1]v "+
+			"dbname=%[2]v sslmode=disable", dbuser, dbname)
+		fmt.Println("Connstr for dev:", ConnStr)
+	} else if url != "" {
+		ConnStr := url
+		fmt.Println("Connstr for prod:", ConnStr)
+	}
 	return ConnStr
 }
