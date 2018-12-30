a8f807d87a27bf4a2355301b1faa47d29f5235a7
Robert Fischer <artiefischer@gmail.com>
2018-12-30 10:00:35 -0700

    working on labels

--- db/dbInit.go
+++ db/dbInit.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/subosito/gotenv"
@@ -14,19 +15,19 @@ var ConnStr = DBInit()
 
 // DBInit initializes the databasw using .env vars
 //FOR DEVELEOPMENT ONLY !!!!
-// func DBInit() string {
-// 	gotenv.Load()
-// 	dbname := os.Getenv("DBNAME")
-// 	dbuser := os.Getenv("DBUSER")
-// 	ConnStr := fmt.Sprintf("user=%[1]v "+
-// 		"dbname=%[2]v sslmode=disable", dbuser, dbname)
-// 	return ConnStr
-// }
-
-// FOR PRODUCTION BUILD ONLY!!!!
 func DBInit() string {
 	gotenv.Load()
-	url := os.Getenv("DATABASE_URL")
-	ConnStr := url
+	dbname := os.Getenv("DBNAME")
+	dbuser := os.Getenv("DBUSER")
+	ConnStr := fmt.Sprintf("user=%[1]v "+
+		"dbname=%[2]v sslmode=disable", dbuser, dbname)
 	return ConnStr
 }
+
+// FOR PRODUCTION BUILD ONLY!!!!
+// func DBInit() string {
+// 	gotenv.Load()
+// 	url := os.Getenv("DATABASE_URL")
+// 	ConnStr := url
+// 	return ConnStr
+// }
